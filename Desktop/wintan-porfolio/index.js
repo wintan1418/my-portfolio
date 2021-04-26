69f1b7cff8fe4b05242717e68fa302938646341d
oluwadare juwon <wintan1418@gmail.com>
2021-04-26 14:23:23 +0100

    add javascript function

---
+++ Desktop/wintan-porfolio/index.js
@@ -0,0 +1,14 @@
+function showabout(){
+    $("#about_container").css("display","inherit");
+    $("#about_container").addClass("animated slideInLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideInLeft");
+    },800);
+}
+function closeabout(){
+    $("#about_container").addClass("animated slideOutLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideOutLeft");
+        $("#about_container").css("display","none");
+    },800);
+}
